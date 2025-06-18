d23966a953bc3fed566307daaaac629a0fdff903
Zeeshan Arain <gulldivecoding@outlook.com>
2025-06-18 04:21:54 +0300

    Added features:  - functionality for buttons open file, save file, revert, new file.  - the user interface updates itself based on the edit status of the file.  - existing files can be opened with drag and drop
    Missing features / Known issues:
     - when dragging and dropping file, it is treated always as a new file.
     - when file is edited by another application, such edits are not monitored.

--- app/main.js
+++ app/main.js
@@ -2,6 +2,35 @@ const { app, BrowserWindow, dialog, ipcMain } = require('electron');
 const path = require('node:path');
 const fs = require('fs');
 const windows = new Set();
+const openedFiles = new Map();
+
+const updateUserInterface = (filePath, targetWindow, isEdited) => {
+  const title = 'Simple Markdown Editor';
+
+  if (filePath === null) {
+    // invoked from keyup event on ui textarea
+    if (isEdited) {
+      // file is opened and it is now edited
+      // add (Edited) in the title if is does not already exist there
+      let currentTitle = targetWindow.title;
+      if (currentTitle.search('Edited') < 0) {
+        currentTitle = currentTitle.replace(title, `(Edited) - ${title}`);
+        targetWindow.setTitle(currentTitle);
+      }
+    } else {
+      // file is opened but is not edited
+      // if (Edited) is found in the title remove it.
+      let currentTitle = targetWindow.title;
+      if (currentTitle.search('Edited') > 0) {
+        currentTitle = currentTitle.split('(Edited) - ').join('');
+        targetWindow.setTitle(currentTitle);
+      }
+    }
+  } else {
+    // invoked from open file dialog
+    if (filePath) { targetWindow.setTitle(`${path.basename(filePath)} - ${title}`); }
+  }
+};
 
 const getFileFromUser = async () => {
   const targetWindow = BrowserWindow.getFocusedWindow();
@@ -19,11 +48,89 @@ const getFileFromUser = async () => {
         if (!rsp.canceled) {
           const file = rsp.filePaths[0];
           const content = fs.readFileSync(file).toString();
-          resolve(content);
+          updateUserInterface(file, targetWindow, null);
+          targetWindow.setRepresentedFilename(file); // macOS specific, others no effect
+          startWatchingFile(targetWindow, file);
+          resolve({ file, content});
         }
       })
       .catch((err) => reject(err));
   });
+};
+
+const saveHtml = async (content) => {
+  const targetWindow = BrowserWindow.getFocusedWindow();
+  const file = dialog.showSaveDialog(targetWindow, {
+    title: 'Save HTML',
+    defaultPath: app.getPath('documents'),
+    filters: [
+      { name: 'HTML Files', extensions: ['html', 'htm'] }
+    ]
+  });
+  return new Promise((resolve, reject) => {
+    file
+      .then((rsp) => {
+        if (rsp.canceled) { resolve({ status: true, msg: 'user-closed-dialog' }); }
+        try {
+          fs.writeFileSync(rsp.filePath, content);
+          resolve({ status: true, msg: 'ok' });
+        }
+        catch { resolve({ status: false, msg: 'could-not-write-file' }); }
+      })
+      .catch(err => { reject({ status: false, msg: 'could-not-open-dialog' }) });
+  });
+};
+
+const saveFile = async (filePath, content) => {
+  const targetWindow = BrowserWindow.getFocusedWindow();
+  return new Promise((resolve, reject) => {
+    if (filePath) {
+      try {
+        fs.writeFileSync(filePath,content);
+        resolve({ status: 'file-edits-saved', filePath: null });
+      } catch {
+        reject("could not save changes in the existing file");
+      }
+
+    } else {
+      const file = dialog.showSaveDialog(targetWindow, {
+        title: 'Save Markdown',
+        defaultPath: app.getPath('documents'),
+        filters: [
+          { name: 'Markdown Files', extensions: ['md', 'markdown'] }
+        ]
+      });
+      file
+        .then((rsp) => {
+          if (!rsp.canceled) {
+            fs.writeFileSync(rsp.filePath, content);
+            resolve({ status: 'new-file-saved', filePath: rsp.filePath });
+          } else {
+            reject({status: 'user-closed-dialog', filePath: null});
+          }
+        })
+        .catch((err) => { reject('could not save changes in the new file') });
+    }
+  });
+};
+
+const startWatchingFile = (targetWindow, file) => {
+  stopWatchingFile(targetWindow);
+  const watcher = fs.watchFile(file, (event) => {
+    if (event === 'change') {
+      const content = fs.readFileSync(file);
+      // find equivalent: send file content to renderer
+      // targetWindow.webContents.send('file-opened', file, content);
+    }
+  });
+  openedFiles.set(targetWindow, watcher);
+};
+
+const stopWatchingFile = (targetWindow) => {
+  if (openedFiles.has(targetWindow)) {
+    openedFiles.get(targetWindow).stop();
+    openedFiles.delete(targetWindow);
+  }
 };
 
 ipcMain.on('create-new-window', (event) => {
@@ -32,6 +139,28 @@ ipcMain.on('create-new-window', (event) => {
 
 ipcMain.handle('getFileFromUser', async (event) => {
   const result = await getFileFromUser();
+  return result;
+});
+
+ipcMain.handle('updateUserInterface', async (event, isEdited) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const targetWindow = BrowserWindow.getFocusedWindow();
+      updateUserInterface(null, targetWindow, isEdited)
+      resolve("ok");
+    } catch (error) {
+      reject(error);
+    }
+  });
+});
+
+ipcMain.handle('saveHtml', async (event, content) => {
+  const result = await saveHtml(content);
+  return result; 
+});
+
+ipcMain.handle('saveFile', async (event, filePath, content) => {
+  const result = await saveFile(filePath, content);
   return result;
 });
 
@@ -66,13 +195,18 @@ const createWindow = () => {
     windows.delete(newWindow);
     newWindow = null;
   });
+  newWindow.on('closed', () => {
+    windows.delete(newWindow);
+    stopWatchingFile(newWindow);
+    newWindow = null;
+  });
   windows.add(newWindow);
   return newWindow;
 
 };
 
 app.on('ready', () => {
-    createWindow();
+  createWindow();
 });
 
 app.on('window-all-closed', () => {
@@ -83,3 +217,17 @@ app.on('window-all-closed', () => {
 app.on('activate', (event, hasVisibleWindows) => {
   if (!hasVisibleWindows) { createWindow(); }
 });
+
+
+/*
+ * Features not supported by Linux
+ * clicking on a file in recent files does not fire the open-file
+ * event.
+
+app.on('will-finish-launching', () => {
+  app.on('open-file', (event, file) => {
+  });
+});
+
+ *
+ */
--- app/renderer.js
+++ app/renderer.js
@@ -8,28 +8,109 @@ const saveHtmlBtn = document.querySelector('#save-html');
 const showFileBtn = document.querySelector('#show-file');
 const openInDefaultBtn = document.querySelector('#open-in-default');
 
+let filePath = null;
+let originalContent = '';
+
 const renderMarkdownToHtml = (markdown) => {
     htmlView.innerHTML = nodeDeps.ndMarked(markdown, { sanitize: true });
 };
 
 markdownView.addEventListener('keyup', (event) => {
     const currentContent = event.target.value;
+    const isEdited = currentContent !== originalContent;
     renderMarkdownToHtml(currentContent);
+    elcMods.invokeAtMain(['updateUserInterface', isEdited])
+        .catch(err => console.error("user interface title not updated"));
+    saveMarkdownBtn.disabled = !isEdited;
+    revertBtn.disabled = !isEdited;
 });
 
 openFileBtn.addEventListener('click', () => {
     elcMods.invokeAtMain(['getFileFromUser'])
         .then((rsp) => {
-            const fileContents = rsp;
-            markdownView.value = fileContents;
-            renderMarkdownToHtml(fileContents);
+            filePath = rsp.file;
+            originalContent = rsp.content;
+            markdownView.value = rsp.content;
+            renderMarkdownToHtml(rsp.content);
         })
         .catch((err) => {
-            console.err(err);
+            console.log(err);
             alert("could not open the file using file explorer");
         });
 });
 
 newFileBtn.addEventListener('click', () => {
     elcMods.sendToMain('create-new-window');
+});
+
+saveHtmlBtn.addEventListener('click', () => {
+    elcMods.invokeAtMain(['saveHtml', htmlView.innerHTML])
+        .then((rsp) => { console.log(rsp); })
+        .catch((err) => { console.error(err); });
+});
+
+saveMarkdownBtn.addEventListener('click', () => {
+    elcMods.invokeAtMain(['saveFile', filePath, markdownView.value])
+        .then((rsp) => { console.log(rsp); })
+        .catch((err) => { console.error(err); });
+});
+
+revertBtn.addEventListener('click', () => {
+    markdownView.value = originalContent;
+    renderMarkdownToHtml(originalContent);
+    const isEdited = false; // because we are reverting
+    elcMods.invokeAtMain(['updateUserInterface', isEdited])
+        .catch(err => console.error("user interface title not updated"));
+    saveMarkdownBtn.disabled = !isEdited;
+    revertBtn.disabled = !isEdited;
+});
+
+// drag and drop feature implementation
+/**
+ * show somewhere on the UI the name of the dropped file
+**/
+document.addEventListener('dragstart', event => event.preventDefault());
+document.addEventListener('dragover', event => event.preventDefault());
+document.addEventListener('dragleave', event => event.preventDefault());
+document.addEventListener('drop', event => event.preventDefault());
+
+const getDraggedFile = (event) => event.dataTransfer.items[0];
+const getDroppedFile = (event) => event.dataTransfer.files[0];
+
+const fileTypeIsSupported = (file) => {
+    return ['text/plain', 'text/markdown'].includes(file.type);
+};
+
+markdownView.addEventListener('dragover', (event) => {
+    const file = getDraggedFile(event);
+    if (fileTypeIsSupported(file)) {
+        markdownView.classList.add('drag-over');
+    } else {
+        markdownView.classList.add('drag-error');
+    }
+});
+
+markdownView.addEventListener('dragleave', () => {
+    markdownView.classList.remove('drag-over');
+    markdownView.classList.remove('drag-error');
+});
+
+markdownView.addEventListener('drop', (event) => {
+    const file = getDroppedFile(event);
+    if (fileTypeIsSupported(file)) {
+        const reader = new FileReader();
+        reader.onload = () => {
+            originalContent = reader.result;
+            markdownView.value = reader.result;
+            renderMarkdownToHtml(reader.result);
+        };
+        reader.onerror = () => {
+            console.error("could not read contents of the dropped file");
+        };
+        reader.readAsText(file);
+    } else {
+        alert ('Dropped file type is not supported');
+    }
+    markdownView.classList.remove('drag-over');
+    markdownView.classList.remove('drag-error');
 });
